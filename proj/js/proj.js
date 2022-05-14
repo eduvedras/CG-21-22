9524399f77e8b62df84aa65002feb4c5b2afe045
eduvedras <edu.vedras@gmail.com>
2022-05-14 16:13:47 +0100

    Mudanças na rotacao

--- proj/js/proj.js
+++ proj/js/proj.js
@@ -1,12 +1,13 @@
 /*global THREE, requestAnimationFrame, console*/
 
 var LateralCamera, FrontalCamera, TopCamera, cameraInUse, scene, renderer;
+var g0, g1, g2, g3, g4, g5, g6, g7;
 
 var geometry, material, mesh;
 
 var ball;
 
-function createCube(x, y, z, width, height, depth) {
+function createCube(x, y, z, width, height, depth, g) {
     'use strict';
 
     var cube = new THREE.Object3D();
@@ -15,62 +16,17 @@ function createCube(x, y, z, width, height, depth) {
 
     geometry = new THREE.CubeGeometry(width, height, depth);
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
+    /*mesh.position.set(x, y, z);*/
     cube.add(mesh);
-
-    scene.add(cube);
     
     cube.position.x = x;
     cube.position.y = y;
     cube.position.z = z;
+
+    g.add(cube);
 }
 
-function addTableTop(obj, x, y, z) {
-    'use strict';
-    geometry = new THREE.CubeGeometry(60, 2, 20);
-    mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
-}
-
-function createBall(x, y, z) {
-    'use strict';
-    
-    ball = new THREE.Object3D();
-    ball.userData = { jumping: true, step: 0 };
-    
-    material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-    geometry = new THREE.SphereGeometry(4, 10, 10);
-    mesh = new THREE.Mesh(geometry, material);
-    
-    ball.add(mesh);
-    ball.position.set(x, y, z);
-    
-    scene.add(ball);
-}
-
-
-function createTable(x, y, z) {
-    'use strict';
-    
-    var table = new THREE.Object3D();
-    
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-   
-    addTableTop(table, 0, 0, 0);
-    addTableLeg(table, -25, -1, -8);
-    addTableLeg(table, -25, -1, 8);
-    addTableLeg(table, 25, -1, 8);
-    addTableLeg(table, 25, -1, -8);
-    
-    scene.add(table);
-    
-    table.position.x = x;
-    table.position.y = y;
-    table.position.z = z;
-}
-
-function createTorus(x, y, z) {
+function createTorus(x, y, z, g) {
     'use strict';
     
     var torus = new THREE.Object3D();
@@ -84,17 +40,17 @@ function createTorus(x, y, z) {
    
     geometry = new THREE.TorusGeometry(radius, tubeRadius, radialSegments, tubularSegments);
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
+    /*mesh.position.set(x, y, z);*/
     torus.add(mesh);
-    
-    scene.add(torus);
     
     torus.position.x = x;
     torus.position.y = y;
     torus.position.z = z;
+
+    g.add(torus);
 }
 
-function createCylinder(x, y, z, len, rot) {
+function createCylinder(x, y, z, len, rot, g) {
     'use strict';
     
     var cylinder = new THREE.Object3D();
@@ -109,15 +65,17 @@ function createCylinder(x, y, z, len, rot) {
     const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
    
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
-    mesh.rotation.x = rot;
+    /*mesh.position.set(x, y, z);
+    mesh.rotation.x = rot;*/
     cylinder.add(mesh);
-    
-    scene.add(cylinder);
+
+    cylinder.rotation.x = rot;
     
     cylinder.position.x = x;
     cylinder.position.y = y;
     cylinder.position.z = z;
+
+    g.add(cylinder);
 
 }
 
@@ -128,12 +86,47 @@ function createScene() {
     
 
     scene.add(new THREE.AxisHelper(10));
+
+    g0 = new THREE.Object3D();
+    g1 = new THREE.Object3D();
+    g2 = new THREE.Object3D();
+    g3 = new THREE.Object3D();
+    g4 = new THREE.Object3D();
+    g5 = new THREE.Object3D();
+    /*g6 = new THREE.Object3D();
+    g7 = new THREE.Object3D();
+    g8 = new THREE.Object3D();*/
     
-    createTorus(0, 0, 5);
-    createCylinder(0, 0, 0, 20, Math.PI / 2);
-    createCube(0,0,-6.3,20,5,5);
-    createCylinder(0, 7.5, -6.3, 25, 0);
-    createCube(0,15,-6.3,5,5,5);
+    createTorus(0, 0, 10, g0);
+    createCylinder(0, 0, 0, 20, Math.PI/2, g0);
+    g0.position.set(0,0,12.5);
+    
+    g1.add(g0);
+    createCube(0,0,0,20,5,5,g1);
+    createCylinder(0, 0, -7.5, 10, Math.PI / 2, g1);
+    createCube(0,0,-15,5,5,5,g1);
+    g1.position.set(0,-2.5,0);
+
+    g2.add(g1);
+    createCylinder(0, 12.5, 0, 25, 0, g2);
+    g2.rotation.y= -Math.PI/4;
+    g2.position.set(0,-27.5,0);
+
+    g3.add(g2);
+    createCube(0,0,0,5,5,5, g3);
+    createCylinder(0,0,-12.5,20, Math.PI / 2, g3);
+    g3.position.set(0,0,27.5);
+
+    g4.add(g3);
+    createCube(0,0,0,20,10,10,g4);
+    createCylinder(0,20,0,30, 0,g4);
+    //g4.rotation.y possivel
+    g4.position.set(0,-40,0);
+
+    g5.add(g4)
+    createCube(0,0,0,10,10,10,g5);
+    g5.position.set(0,30,0);//temp
+    scene.add(g5);
 }
 
 function createCamera() {
